02179b5c1cd3072096dee94cf0f9a7b8700f576f
VoxiByte <services@voxibyte.com>
2023-11-13 15:58:21 +0100

    Initial Commit

---
+++ src/main/java/it/voxibyte/privateislands/PrivateIslands.java
@@ -0,0 +1,16 @@
+package it.voxibyte.privateislands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrivateIslands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
